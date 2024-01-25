7c7f24058b5b4960df088530b51d9e80a9d2c439
JoelKuruvill <78065567+JoelKuruvill@users.noreply.github.com>
2024-01-25 02:07:23 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 #print("Hello World!")
 
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-  return "Hello World!"
+  #return "Hello World!"
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run(host='0.0.0.0', debug=True)  #port=8080
   #print("I'm inside the if")
